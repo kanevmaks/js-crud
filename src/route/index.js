a57fd1c83918822356140e4065f18930d095cfb8
(makskanev) <129537315+makskanev@users.noreply.github.com>
2023-12-31 20:49:50 +0100

    created user crud

--- src/route/index.js
+++ src/route/index.js
@@ -1,7 +1,87 @@
 // Підключаємо технологію express для back-end сервера
+const e = require('express')
 const express = require('express')
 // Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
 const router = express.Router()
+
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.get('/', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  const list = User.getList()
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('index', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'index',
+
+    data: {
+      users: {
+        list: User.getList(),
+        isEmpty: list.length === 0,
+      },
+    },
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
+class User {
+	static #list = []
+  
+	constructor(email, login, password) {
+	  this.email = email
+	  this.login = login
+	  this.password = password
+	  this.id = new Date().getTime()
+	}
+  
+	verifyPassword = (password) => this.password === password
+  
+	static add = (user) => {
+	  this.#list.push(user)
+	}
+  
+	static getList = () => this.#list
+  
+	static getById = (id) =>
+	  this.#list.find((user) => user.id === id)
+  
+	static deleteById = (id) => {
+	  const index = this.#list.findIndex(
+		(user) => user.id === id,
+	  )
+  
+	  if (index !== -1) {
+		this.#list.splice(index, 1)
+		return true
+	  } else {
+		return false
+	  }
+	}
+
+	static updateById = (id, data) => {
+		const user = this.getById(id)
+	
+		if (user) {
+		  this.update(user, data)
+		  return true
+		} else {
+		  return false
+		}
+	  }
+	
+	  static update = (user, { email }) => {
+		if (email) {
+		  user.email = email
+		}
+	  }
+}
 
 // ================================================================
 
@@ -21,31 +101,63 @@ router.get('/', function (req, res) {
 
 // ================================================================
 
-class User {
-  #list = []
-
-  constructor(email, login, password) {
-    this.email = email
-    this.login = login
-    this.password = password
-  }
-
-  static create = (user) => {
-    this.тутlist.push(user)
-  }
-
-  static getList
-}
-
-// ================================================================
-
 router.post('/user-create', function (req, res) {
-  console.log(req.body)
-  res.render('user-create', {
-    style: 'user-create',
+  const { email, login, password } = req.body
+
+  const user = new User(email, login, password)
+
+  User.add(user)
+
+  console.log(User.getList())
+
+  res.render('success-info', {
+    style: 'success-info',
+	info: 'Користувач створенний'
   })
 })
 // Підключаємо роутер до бек-енду
 module.exports = router
 
 // ================================================================
+
+
+router.get('/user-delete', function (req, res) {
+	const { id } = req.query
+
+
+	User.deleteById(Number(id))
+  
+	res.render('success-info', {
+	  style: 'success-info',
+	  info: 'Користувач видаленний'
+	})
+  })
+  // Підключаємо роутер до бек-енду
+  module.exports = router
+  
+  // ================================================================
+  
+  router.post('/user-update', function (req, res) {
+  const { email, password, id } = req.body
+
+  let result = false
+
+  const user = User.getById(Number(id))
+
+  if (user.verifyPassword(password)) {
+    User.update(user, { email })
+    result = true
+  }
+
+  res.render('success-info', {
+    style: 'success-info',
+    info: result
+      ? 'Email пошта оновлена'
+      : 'Сталася помилка',
+  })
+})
+
+// Підключаємо роутер до бек-енду
+module.exports = router
+  
+  // ================================================================
